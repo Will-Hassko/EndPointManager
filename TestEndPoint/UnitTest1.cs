c50dcb359bb5f4bb13e0db426ac94e5be25c1b75
Willyan Hassko <will.hassko@outlook.com>
2024-07-07 18:24:16 +0100

    Adding Test Project

---
+++ TestEndPoint/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestEndPoint
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
